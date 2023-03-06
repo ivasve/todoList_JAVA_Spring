3abefd0b0e26e50af279f5de24917274eaa78cb4
Ivana Jones <ivana.jones@plus4u.net>
2023-03-06 15:19:39 +1100

    rename Tests.java

--- src/test/java/com/example/todoList/todoListApplicationTests.java
+++ src/test/java/com/example/todoList/todoListApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatGpt01ApplicationTests {
+class todoListApplicationTests {
 
     @Test
     void contextLoads() {
