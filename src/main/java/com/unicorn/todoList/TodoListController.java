063b84d1be39509246759414b26b36c7329c03e5
Ivana Jones <ivana.jones@plus4u.net>
2023-03-27 14:34:51 +1100

    add Service class, and basic Security

--- src/main/java/com/unicorn/todoList/TodoListController.java
+++ src/main/java/com/unicorn/todoList/TodoListController.java
@@ -1,9 +1,11 @@
-package com.example.todoList;
+package com.unicorn.todoList;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,32 +21,33 @@ public class TodoListController {
 
     @Autowired
     private TodoListRepository todoListRepository;
+    @Autowired
+    private ToDoListService toDoListService;
 
     @GetMapping("/list")
     public Iterable<TodoListEntity> findAll() {
-        return todoListRepository.findAll();
+        return toDoListService.returnAllTodoList();
     }
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.OK)
     public TodoListEntity create(@RequestBody TodoListEntity todo) {
-        return todoListRepository.save(todo);
+        return toDoListService.createTodoList(todo);
 
     }
 
     @PutMapping("/{id}/update")
     @ResponseStatus(HttpStatus.OK)
     public TodoListEntity update(@PathVariable Long id, @RequestBody TodoListEntity todo) {
-        TodoListEntity existingTodo = todoListRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Todo not found"));
-        existingTodo.setText(todo.getText());
-//        existingTodo.setCompleted(todo.isCompleted());
-        return todoListRepository.save(existingTodo);
+        toDoListService.updateTodoList(id, todo);
+        return toDoListService.updateTodoList(id, todo);
     }
 
     @DeleteMapping("/{id}/delete")
+    @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Long id) {
-        TodoListEntity todo = todoListRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Todo not found"));
-        todoListRepository.delete(todo);
+        toDoListService.deleteTodoList(id);
     }
+
 }
 
